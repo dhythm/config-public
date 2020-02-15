59d282022683048f6aadf558b78a4bb67c532d1d
dhythm <Yuta.Okada.20@gmail.com>
2020-02-15 09:41:42 +0800

    update distribute_config

--- distribute_config.sh
+++ distribute_config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cp -f vimrc ~/.vimrc
-cp -f vimrc /Applications/MacVim.app/Contents/Resources/vim/vimrc
-cp -f gvimrc /Applications/MacVim.app/Contents/Resources/vim/gvimrc
-cp -f .zshrc ~/.zshrc
+\cp -f vimrc ~/.vimrc
+\cp -f vimrc /Applications/MacVim.app/Contents/Resources/vim/vimrc
+\cp -f gvimrc /Applications/MacVim.app/Contents/Resources/vim/gvimrc
+\cp -f .zshrc ~/.zshrc
 
